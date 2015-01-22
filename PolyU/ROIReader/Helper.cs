15d2508029325dcd3c0345006694ef73a0a3b80f
kingofcrabs <rex.xie@qq.com>
2015-01-22 17:02:51 +0800

    create

---
+++ PolyU/ROIReader/Helper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROIReader
+{
+    public class Helper
+    {
+        static public string GetExeFolder()
+        {
+            string s = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return s + "\\";
+        }
+
+        static public string GetExeParentFolder()
+        {
+            string s = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            int index = s.LastIndexOf("\\");
+            return s.Substring(0, index) + "\\";
+        }
+
+        public static string GetOutputFolder()
+        {
+            string sOutputFolder = GetExeParentFolder() + "Output\\";
+
+            if (!Directory.Exists(sOutputFolder))
+            {
+                Directory.CreateDirectory(sOutputFolder);
+            }
+            return sOutputFolder;
+        }
+
+        public static void WriteResult(bool bok)
+        {
+            string sOutputFolder = GetOutputFolder();
+            string sResultFile = sOutputFolder + "result.txt";
+            File.WriteAllText(sResultFile, bok.ToString());
+        }
+
+        public static void WriteErrorMsg(string sMessage)
+        {
+            string sOutputFolder = GetOutputFolder();
+            string sResultFile = sOutputFolder + "errorMsg.txt";
+            File.WriteAllText(sResultFile, sMessage);
+        }
+
+        internal static void WriteAvg(double avg)
+        {
+            string sOutputFolder = GetOutputFolder();
+            string sAvgFile = sOutputFolder + "average.txt";
+            File.WriteAllText(sAvgFile, avg.ToString());
+        }
+
+        internal static void WriteValue(List<string> values)
+        {
+            string sOutputFolder = GetOutputFolder();
+            Console.WriteLine("write to: " + sOutputFolder + "values.txt");
+            File.WriteAllLines(sOutputFolder + "values.txt",values);
+        }
+    }
+}
---
+++ PolyU/ROIReader/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ROIReader
+{
+    class Program
+    {
+        static string version = "0.01";
+        static void Main(string[] args)
+        {
+            Console.WriteLine(version);
+            Helper.WriteResult(false);
+            if(args.Count() != 2)
+            {
+                Console.WriteLine("Arguments count must be 2!\r\n Press any key to exit");
+                Console.ReadKey();
+                return;
+            }
+            int batchNum = 0;
+            bool bok =  int.TryParse(args[1], out batchNum);
+            if(!bok)
+            {
+                Console.WriteLine("Second argument must be integer!\r\n Press any key to exit");
+                Console.ReadKey();
+                return;
+            }
+            try
+            {
+                string folder = ConfigurationManager.AppSettings["workingFolder"];
+                AscFile ascFile = new AscFile(string.Format("{0}{1}.asc",folder,args[0]));
+                int wellCntInBatch = int.Parse(ConfigurationManager.AppSettings["wellsPerTime"]);
+                List<double> vals = ascFile.GetBatch(batchNum, wellCntInBatch);
+                Helper.WriteValue(vals.Select(x => x.ToString()).ToList());
+                bool bRemoveSmallest = bool.Parse(ConfigurationManager.AppSettings["removeSmallest"]);
+
+                if (bRemoveSmallest)
+                    vals.Remove(vals.Min());
+                double avg = vals.Average();
+                Helper.WriteAvg(Math.Round(avg,4));
+            }
+            catch(Exception ex)
+            {
+                 Console.WriteLine( ex.Message + "\r\n Press any key to exit");
+                 Console.ReadKey();
+                 return;
+            }
+            Helper.WriteResult(true);
+        }
+    }
+
+
+    class AscFile
+    {
+        Dictionary<int, double> wellPos_Val = new Dictionary<int, double>();
+        public AscFile(string file)
+        {
+            List<string> strs = new List<string>();
+            strs = File.ReadAllLines(file).ToList();
+            strs = strs.Where(x => x != "").ToList();
+            if(strs.Count != 9)
+            {
+                throw new Exception("lines in the file != 9");
+            }
+            strs.RemoveAt(0);
+            for(int row = 0; row < 8; row++)
+            {
+                List<string> sColumns = strs[row].Split('\t').ToList();
+                sColumns.RemoveAt(0);
+                for( int col = 0; col < sColumns.Count; col++)
+                {
+                    int wellID = col * 8 + row + 1;
+                    string sVal = sColumns[col];
+                    if (sVal == "")
+                        continue;
+                    double val = double.Parse(sVal);
+                    wellPos_Val.Add(wellID, val);
+                }
+            }
+        }
+
+
+        internal List<double> GetBatch(int batchNum, int wellCntInBatch)
+        {
+            int wellIDStart = (batchNum - 1) * wellCntInBatch + 1;
+            int wellIDEnd = wellIDStart + wellCntInBatch - 1;
+            return wellPos_Val.Where(x => x.Key >= wellIDStart && x.Key <= wellIDEnd).Select(x => x.Value).ToList();
+        }
+    }
+}
