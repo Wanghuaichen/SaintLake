68893c3c37c465ed7a0682b35f0e62169d176838
kingofcrabs <rex.xie@qq.com>
2015-01-23 08:06:49 +0800

    about form

---
+++ SaintX/SaintX/HelpForm.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using SaintX.Properties;
+
+namespace SaintX
+{
+    /// <summary>
+    /// HelpForm.xaml 的交互逻辑
+    /// </summary>
+    public partial class HelpForm : Window
+    {
+        public HelpForm()
+        {
+            InitializeComponent();
+            lblDescription.Content = "SaintX:版本号：" + stringRes.version;
+        }
+    }
+}
